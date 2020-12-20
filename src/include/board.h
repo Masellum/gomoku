4f836e1c0c10ab1b2ad04a0d002fc904bf6c2f35
Hanwen Guo <hanwen.guo@whu.edu.cn>
2020-12-20 19:07:51 +0800

    fix(fix twodimentionalarray pointer parameter): fix the parameter bug of several function

--- src/include/board.h
+++ src/include/board.h
@@ -1,6 +1,8 @@
 #ifndef _BOARD_H
 #define _BOARD_H
 
+#include "AI.h"
+
 typedef int (*twoDimensionalArray)[15];
 
 /**
@@ -13,35 +15,35 @@ twoDimensionalArray initBoard();
  * Fill the board with zeros.
  * @param board the pointer points to the board
  */
-void clearBoard(twoDimensionalArray *board);
+void clearBoard(twoDimensionalArray board);
 
 /**
  * Free the memories allocated by initBoard().
  */
-void deleteBoard(twoDimensionalArray *board);
+void deleteBoard(twoDimensionalArray board);
 
 /**
  * 在坐标为 (x，y) 的位置放下一颗棋子
  * <br />
  * Put a chess at (x, y).
  */
-void putChess(twoDimensionalArray *board, int player, int x, int y);
+void putChess(twoDimensionalArray board, int player, int x, int y);
 
 /**
  * 检查在坐标为 (x, y) 的位置放下棋子后是否能获胜
  */
-int checkWinOrNotAtPosition(twoDimensionalArray *board, int x, int y);
+int checkWinOrNotAtPosition(twoDimensionalArray board, int x, int y, int player);
 
 /**
  *
  * @return
  */
-int checkWinOrNot(twoDimensionalArray *board);
+int checkWinOrNot(twoDimensionalArray board);
 
 /**
  *
  */
-int checkIfBoardIsFull(twoDimensionalArray *board);//检查棋盘是否填满
+int checkIfBoardIsFull(twoDimensionalArray board);//检查棋盘是否填满
 void recordPutChess(char *type, int player, int a, int b);//记录在（a，b）位置下了一颗棋子
 int checkPositionAvailable(int a, int b);//返回在（a，b）落子是否符合规范，如果不符合，打印错误信息
 
